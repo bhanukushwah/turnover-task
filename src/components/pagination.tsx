2854d2f935d7d5fb4914e897783281bfcc1b8ddb
bhanukushwah <bhanupskofficial@gmail.com>
2024-03-24 23:31:15 +0530

    added option to show maximum pages in the pagination

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -13,24 +13,47 @@ const Pagination: React.FC<PaginationProps> = ({
 }) => {
     const renderPageNumbers = () => {
         const pageNumbers = [];
-        for (let i = 1; i <= totalPages; i++) {
+        const startPage = Math.max(1, currentPage - 5); // Start from currentPage - 5
+        const endPage = Math.min(totalPages, startPage + 9); // End at startPage + 9 or totalPages
+
+        for (let i = startPage; i <= endPage; i++) {
             pageNumbers.push(
                 <li key={i} className={i === currentPage ? 'active' : 'text-gray-400'}>
-                    <button onClick={() => onPageChange(i)}>{i}</button>
+                    <button onClick={() => onPageChange(i)} aria-current={currentPage === i ? 'page' : undefined}>{i}</button>
                 </li>
             );
         }
+
+        if (totalPages > 10) {
+            // Display ellipsis if totalPages is greater than 10
+            if (startPage > 1) {
+                pageNumbers.unshift(
+                    <li key="left-ellipsis">
+                        <span>...</span>
+                    </li>
+                );
+            }
+            if (endPage < totalPages) {
+                pageNumbers.push(
+                    <li key="right-ellipsis">
+                        <span>...</span>
+                    </li>
+                );
+            }
+        }
+
         return pageNumbers;
     };
 
     return (
-        <div className='py-4'>
+        <nav className='py-4'>
             <ul className="flex gap-4">
                 <li>
                     <button
                         className='text-gray-400'
                         disabled={currentPage === 1}
                         onClick={() => onPageChange(1)}
+                        aria-label="First Page"
                     >
                         {'<<'}
                     </button>
@@ -40,6 +63,7 @@ const Pagination: React.FC<PaginationProps> = ({
                         className='text-gray-400'
                         disabled={currentPage === 1}
                         onClick={() => onPageChange(currentPage - 1)}
+                        aria-label="Previous Page"
                     >
                         {'<'}
                     </button>
@@ -50,6 +74,7 @@ const Pagination: React.FC<PaginationProps> = ({
                         className='text-gray-400'
                         disabled={currentPage === totalPages}
                         onClick={() => onPageChange(currentPage + 1)}
+                        aria-label="Next Page"
                     >
                         {'>'}
                     </button>
@@ -59,12 +84,13 @@ const Pagination: React.FC<PaginationProps> = ({
                         className='text-gray-400'
                         disabled={currentPage === totalPages}
                         onClick={() => onPageChange(totalPages)}
+                        aria-label="Last Page"
                     >
                         {'>>'}
                     </button>
                 </li>
             </ul>
-        </div>
+        </nav>
     );
 };
 
